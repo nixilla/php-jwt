e484a9fd556ccd66bfbb7c49718c53c172ead4bf
Janusz Slota <janusz.slota@nixilla.com>
2013-05-21 14:50:12 +0100

    Removed tabs

--- test/JWT/Test/Authentication/JWTTest.php
+++ test/JWT/Test/Authentication/JWTTest.php
@@ -6,41 +6,41 @@ use JWT\Authentication\JWT;
 
 class JWTTest extends \PHPUnit_Framework_TestCase
 {
-	function testEncodeDecode()
+    public function testEncodeDecode()
     {
-		$msg = JWT::encode('abc', 'my_key');
-		$this->assertEquals(JWT::decode($msg, 'my_key'), 'abc');
-	}
+        $msg = JWT::encode('abc', 'my_key');
+        $this->assertEquals(JWT::decode($msg, 'my_key'), 'abc');
+    }
 
-	function testDecodeFromPython()
+    public function testDecodeFromPython()
     {
-		$msg = 'eyJhbGciOiAiSFMyNTYiLCAidHlwIjogIkpXVCJ9.Iio6aHR0cDovL2FwcGxpY2F0aW9uL2NsaWNreT9ibGFoPTEuMjMmZi5vbz00NTYgQUMwMDAgMTIzIg.E_U8X2YpMT5K1cEiT_3-IvBYfrdIFIeVYeOqre_Z5Cg';
-		$this->assertEquals(
-			JWT::decode($msg, 'my_key'),
-			'*:http://application/clicky?blah=1.23&f.oo=456 AC000 123'
-		);
-	}
+        $msg = 'eyJhbGciOiAiSFMyNTYiLCAidHlwIjogIkpXVCJ9.Iio6aHR0cDovL2FwcGxpY2F0aW9uL2NsaWNreT9ibGFoPTEuMjMmZi5vbz00NTYgQUMwMDAgMTIzIg.E_U8X2YpMT5K1cEiT_3-IvBYfrdIFIeVYeOqre_Z5Cg';
+        $this->assertEquals(
+            JWT::decode($msg, 'my_key'),
+            '*:http://application/clicky?blah=1.23&f.oo=456 AC000 123'
+        );
+    }
 
-	function testUrlSafeCharacters()
+    public function testUrlSafeCharacters()
     {
-		$encoded = JWT::encode('f?', 'a');
-		$this->assertEquals('f?', JWT::decode($encoded, 'a'));
-	}
+        $encoded = JWT::encode('f?', 'a');
+        $this->assertEquals('f?', JWT::decode($encoded, 'a'));
+    }
 
     /**
      * @expectedException \DomainException
      */
-    function testMalformedUtf8StringsFail()
+    public function testMalformedUtf8StringsFail()
     {
         \PHPUnit_Framework_Error_Warning::$enabled = false;
-		@JWT::encode(pack('c', 128), 'a');
-	}
+        @JWT::encode(pack('c', 128), 'a');
+    }
 
     /**
      * @expectedException \DomainException
      */
-	function testMalformedJsonThrowsException()
+    public function testMalformedJsonThrowsException()
     {
-		JWT::jsonDecode('this is not valid JSON string');
-	}
+        JWT::jsonDecode('this is not valid JSON string');
+    }
 }
